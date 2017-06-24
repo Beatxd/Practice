dc7cabf1402d0f6c32e30dbc83a148993a9d02fe
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-06-24 23:08:16 +0300

    OOP. Prototype style. Clock

---
+++ js/practice21.js
@@ -0,0 +1,39 @@
+'use strict';
+function Clock(options) {
+    this._template = options.template;
+}
+Clock.prototype._render = function () {
+    var date = new Date();
+
+    var hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    var min = date.getMinutes();
+    if (min < 10) min = '0' + min;
+
+    var sec = date.getSeconds();
+    if (sec < 10) sec = '0' + sec;
+
+    var output = this._template.replace('h', hours).replace('m', min).replace('s', sec);
+
+    console.log(output);
+};
+Clock.prototype.start = function () {
+    var saveThis = this;
+    if (this._run) throw new Error('clock was already start');
+    this._run = true;
+    this._render();
+    this._timer = setInterval(function () {
+        saveThis._render()
+    }, 1000);
+};
+Clock.prototype.stop = function () {
+    clearInterval(this._timer);
+    this._run = false;
+};
+Clock.prototype._run = false;
+
+var clock = new Clock({
+    template: 'h:m:s'
+});
+clock.start();
