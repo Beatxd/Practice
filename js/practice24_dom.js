587fe1989f480ecb0f8e60e34da8e8c26933a120
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-07-26 22:10:47 +0300

    DOM. calendar(not complite)

--- js/practice24_dom.js
+++ js/practice24_dom.js
@@ -78,9 +78,9 @@ function treeNumDecorator(f) {
     return function (container, obj) {
         f.apply(this, arguments);
         const liArr = container.querySelectorAll('li');
-        liArr.forEach(function(li){
+        liArr.forEach(function (li) {
             let liNum = li.querySelectorAll('li').length;
-            if(liNum && li.firstChild.data.search(/\[\d+\]$/) === -1) {
+            if (liNum && li.firstChild.data.search(/\[\d+\]$/) === -1) {
                 li.firstChild.data += `[${liNum}]`;
             }
         })
@@ -92,4 +92,43 @@ function isObjEmpty(obj) {
         return false;
     }
     return true;
+}
+
+
+// ----------sixth part------------
+
+document.body.appendChild(document.createElement('div')).id = 'calendar';
+createCalendar('calendar', 2012, 9);
+
+function createCalendar(containerId, year, month) {
+    const parent = document.getElementById(containerId);
+    // const date = new Date(year, month - 1);
+    const daysInMonth = 32 - new Date(year, month - 1, 32).getDate();
+    const days = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+    const options = {
+        year: 'numeric',
+        month: 'long',
+    };
+
+    const title = parent.appendChild(document.createElement('h2'));
+    title.innerHTML = new Date(year, month - 1).toLocaleString('ru', options);
+
+    let table = parent.appendChild(document.createElement('table'));
+
+    const tr = newTr();
+    for (let i = 0; i < days.length; i++) {
+        tr.appendChild(document.createElement('th')).innerHTML = days[i];
+    }
+    for (let i = 1; i <= daysInMonth; i++) {
+        console.log(new Date(year, month - 1, i).getDay());
+    }
+
+
+    function newTr() {
+        return table.appendChild(document.createElement('tr'));
+    }
+
+    function newTd(trNode) {
+        return trNode.appendChild(document.createElement('td'));
+    }
 }
