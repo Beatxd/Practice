7d768ef3ead2ffaf0457780c1dd4d57c1ec2c3fc
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-08-02 23:50:39 +0300

    DOM. styles

--- js/practice25.js
+++ js/practice25.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let button = document.createElement('a');
-button. innerHTML = 'Нажми меня';
+button.innerHTML = 'Нажми меня';
 button.style.cssText = '-moz-border-radius: 8px; \
 -webkit-border-radius: 8px; \
 border-radius: 8px; \
@@ -17,4 +17,38 @@ font-weight: bold;';
 
 document.body.appendChild(button);
 
+// ----------second part-----------
 
+/**
+ * Показывает уведомление, пропадающее через 1.5 сек
+ *
+ * @param options.top {number} вертикальный отступ, в px
+ * @param options.right {number} правый отступ, в px
+ * @param options.cssText {string} строка стиля
+ * @param options.className {string} CSS-класс
+ * @param options.html {string} HTML-текст для показа
+ */
+function showNotification(options) {
+    let notification = document.createElement('div');
+
+    if (options.cssText) notification.style.cssText = options.cssText;
+    notification.style.top = options.top + 'px' || 0;
+    notification.style.right = options.right + 'px' || 0;
+    if (options.className) notification.className = options.className;
+    if (options.html) notification.innerHTML = options.html;
+
+    notification.style.position = 'absolute';
+
+    document.body.appendChild(notification);
+
+    setTimeout(function () {
+        notification.parentNode.removeChild(notification);
+    }, 1500)
+}
+
+showNotification({
+    top: 10,
+    right: 10,
+    html: "Привет",
+    className: "welcome"
+});
