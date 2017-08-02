2f4e2c27df1973c35a8c6f6f89313dd29a404a73
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-08-02 23:26:44 +0300

    DOM. styles

---
+++ js/practice25.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let button = document.createElement('a');
+button. innerHTML = 'Нажми меня';
+button.style.cssText = '-moz-border-radius: 8px; \
+-webkit-border-radius: 8px; \
+border-radius: 8px; \
+border: 2px groove green; \
+display: block; \
+height: 30px; \
+line-height: 30px; \
+width: 100px; \
+text-decoration: none; \
+text-align: center; \
+color: red; \
+font-weight: bold;';
+
+document.body.appendChild(button);
+
+
