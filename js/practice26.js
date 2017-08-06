ea604cba83c04149feaa10e92d2be085e5e55fab
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-08-06 23:51:44 +0300

    DOM. Events

--- js/practice26.js
+++ js/practice26.js
@@ -6,6 +6,7 @@ text.innerHTML = 'Hide this text pls. Press the button:';
 text.id = 'text1';
 
 textHider(text1);
+body.appendChild(document.createElement('br'));
 
 function textHider(elem) {
 let button = elem.parentNode.insertBefore(document.createElement('input'), elem.nextElementSibling);
@@ -19,13 +20,40 @@ button.addEventListener('click', function(){
 
 // ----------second part-----------
 
-createOneClickButton('Click for hide me!');
+createOneClickButton('Click to hide me!');
 
 function createOneClickButton(value) {
     let button = body.appendChild(document.createElement('input'));
     button.type = 'button';
     button.value = value;
     button.addEventListener('click', function(){
-        button.hidden = true;
+        this.hidden = true;
+    })
+}
+
+
+// ----------third part-----------
+
+createSweetMenu();
+
+function createSweetMenu() {
+    let ul = document.createElement('ul');
+    ul.innerHTML = 'Сладости (нажми меня)!';
+    ul.className = 'sweetMenu';
+
+    let li = ul.appendChild(document.createElement('li'));
+    li.innerHTML = 'Торт';
+    li = ul.appendChild(document.createElement('li'));
+    li.innerHTML = 'Пончик';
+    li = ul.appendChild(document.createElement('li'));
+    li.innerHTML = 'Пирожное';
+
+    body.appendChild(ul);
+
+    ul.addEventListener('click', function(){
+        let liArr = ul.querySelectorAll('li');
+        for(let i = 0; i < liArr.length; i++){
+            liArr[i].classList.toggle('hide');
+        }
     })
 }
